cb98bf5aa8f3061538d0c0dbdced2d8ff2d739ea
maskman1202 <79311231+maskman1202@users.noreply.github.com>
2021-02-19 20:21:48 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,121 @@
+# risa.ver2
+risaの更新版
+
+
+const http = require("http");
+const querystring = require("querystring");
+const discord = require("discord.js");
+const client = new discord.Client();
+
+http
+  .createServer(function(req, res) {
+    if (req.method == "POST") {
+      var data = "";
+      req.on("data", function(chunk) {
+        data += chunk;
+      });
+      req.on("end", function() {
+        if (!data) {
+          res.end("No post data");
+          return;
+        }
+        var dataObject = querystring.parse(data);
+        console.log("post:" + dataObject.type);
+        if (dataObject.type == "wake") {
+          console.log("Woke up in post");
+          res.end();
+          return;
+        }
+        res.end();
+      });
+    } else if (req.method == "GET") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Discord Bot is active now\n");
+    }
+  })
+  .listen(3000);
+
+
+//おみくじ
+client.on('message', message =>{
+  if (message.author.id == client.user.id || message.author.bot){
+    return;
+  }
+  if (message.content.match(/^！おみくじ/) ||
+      (message.isMemberMentioned(client.user) && message.content.match(/おみくじ/))){
+    let arr = ["大吉", "吉", "凶", "ぽてと", "にゃ～ん", "しゅうまい君"];
+    let weight = [5, 30, 10, 15, 20, 20];
+    lotteryByWeight(message.channel.id, arr, weight);
+  }else if (message.isMemberMentioned(client.user)){
+    sendReply(message, "呼びましたか？");
+  }
+});
+
+function lotteryByWeight(channelId, arr, weight){
+  let totalWeight = 0;
+  for (var i = 0; i < weight.length; i++){
+    totalWeight += weight[i];
+  }
+  let random = Math.floor(Math.random() * totalWeight);
+  for (var i = 0; i < weight.length; i++){
+    if (random < weight[i]){
+      sendMsg(channelId, arr[i]);
+      return;
+    }else{
+      random -= weight[i];
+    }
+  }
+  console.log("lottery error");
+}
+
+
+
+//返事
+client.on("ready", message => {
+  console.log("Bot準備完了～");
+  client.user.setPresence({ game: { name: "描き直し" } });
+});
+
+client.on("message", message => {
+  if (message.author.id == client.user.id || message.author.bot) {
+    return;
+  }
+  if (message.isMemberMentioned(client.user)) {
+    sendReply(message, "どうしましたか？");
+    return;
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "/アマアス募集") {
+    msg.channel.send("アマングアスする人いますか@everyone？");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "ピクトセンス") {
+    msg.channel.send("https://pictsense.com/#!/1613479062405_143");
+  }
+});
+
+if (process.env.DISCORD_BOT_TOKEN == undefined) {
+  console.log("DISCORD_BOT_TOKENが設定されていません。");
+  process.exit(0);
+}
+
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+function sendReply(message, text) {
+  message
+    .reply(text)
+    .then(console.log("リプライ送信: " + text))
+    .catch(console.error);
+}
+
+function sendMsg(channelId, text, option = {}) {
+  client.channels
+    .get(channelId)
+    .send(text, option)
+    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
+    .catch(console.error);
+}
