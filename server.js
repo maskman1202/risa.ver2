6ac53e487aff9c4f517d715764a8cc6f9be8be29
maskman1202 <79311231+maskman1202@users.noreply.github.com>
2021-03-04 21:10:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ http
   })
   .listen(3000);
 
+//ログ
+client.on("ready", message => {
+  console.log("Bot準備完了～");
+  client.user.setPresence({ game: { name: "試験運用中" } });
+});
+
 //ミュートだけできる
 client.on("message", message => {
   if (message.content == "/muteAll") {
@@ -41,59 +47,107 @@ client.on("message", message => {
   }
 });
 
-//おみくじ
-client.on('message', message =>{
-  if (message.author.id == client.user.id || message.author.bot){
-    return;
-  }
-  if (message.content.match(/^！おみくじ/) ||
-      (message.isMemberMentioned(client.user) && message.content.match(/おみくじ/))){
-    let arr = ["大吉", "吉", "凶", "ぽてと", "にゃ～ん", "しゅうまい君"];
-    let weight = [5, 30, 10, 15, 20, 20];
-    lotteryByWeight(message.channel.id, arr, weight);
-  }else if (message.isMemberMentioned(client.user)){
-    sendReply(message, "呼びましたか？");
-  }
-});
-
-function lotteryByWeight(channelId, arr, weight){
-  let totalWeight = 0;
-  for (var i = 0; i < weight.length; i++){
-    totalWeight += weight[i];
-  }
-  let random = Math.floor(Math.random() * totalWeight);
-  for (var i = 0; i < weight.length; i++){
-    if (random < weight[i]){
-      sendMsg(channelId, arr[i]);
-      return;
-    }else{
-      random -= weight[i];
-    }
-  }
-  console.log("lottery error");
-}
 
 
 
-//返事
-client.on("ready", message => {
-  console.log("Bot準備完了～");
-  client.user.setPresence({ game: { name: "描き直し" } });
-});
-
-client.on("message", message => {
-  if (message.author.id == client.user.id || message.author.bot) {
-    return;
-  }
-  if (message.isMemberMentioned(client.user)) {
-    sendReply(message, "どうしましたか？");
-    return;
+//システム
+client.on("message", msg => {
+  if (msg.content === "start") {
+    msg.channel.send("START");
   }
 });
 
 client.on("message", msg => {
+  if (msg.content === "reset") {
+    msg.channel.send("===========================RESTERT===========================");
+  }
+});
+
+//risaの紹介
+client.on("message", msg => {
+  if (msg.content === "risaの紹介") {
+    msg.channel.send(
+      "ただいま試験運用中です、誤作動や過激な発言、エラーなどよく起きると思いますがご了承ください"
+    );
+  }
+});
+
+//募集コード
+client.on("message", msg => {
   if (msg.content === "/アマアス募集") {
     msg.channel.send("アマングアスする人いますか@everyone？");
+  }
+});
+
+//サーバー
+client.on("message", msg => {
+  if (msg.content === "SB-1") {
+    msg.channel.send("Asia");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "SB-2") {
+    msg.channel.send("NorthAmerica");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "SB-3") {
+    msg.channel.send("EuropaEuropa");
+  }
+});
+
+//通話
+client.on("message", msg => {
+  if (msg.content === "AC-1") {
+    msg.channel.send("通話お願いします。");
+  }
+});
+
+//挨拶
+client.on("message", msg => {
+  if (msg.content === "よろしくお願いします") {
+    msg.channel.send("よろしくお願いします！");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "よろしくです") {
+    msg.channel.send("よろしくお願いします！");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "お疲れ様でした") {
+    msg.channel.send("お疲れ様でした！");
+  }
+});
+
+//身内言葉
+client.on("message", msg => {
+  if (msg.content === "なかむ嫌い") {
+    msg.channel.send("それな");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "MASK神") {
+    msg.channel.send("MASKはとても優しい神です");
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content === "ミュートして") {
+    msg.channel.send("は？自分でしろ！！人間やろ");
+  }
+});
+
+//サーバー　検索
+
+client.on("message", msg => {
+  if (msg.content === "招待コード") {
+    msg.channel.send("https://discord.gg/PrdeNXebv2");
   }
 });
 
@@ -102,6 +156,92 @@ client.on("message", msg => {
     msg.channel.send("https://pictsense.com/#!/1613479062405_143");
   }
 });
+
+
+//不適切なメッセージのさくじょ
+client.on('message',message=>{
+ if (message.content.match(/しね|ころす|死ね|殺す|きっしょ|ゴミ|野獣先輩|糞|カス/)) {
+   message.channel.send('不適切な表現が含まれていたため、今後気をつけるようお願いします');
+  message.delete(100)
+}
+})
+client.on('message',message=>{
+if (message.content.match(/不適切な表現が含まれていたため、削除しました。/)) {
+        message.delete(3000)
+   }
+});
+
+
+
+
+//コインゲーム
+client.on('message', message => {
+if(message.content === '！コインゲーム'){
+var array = [":dvd:omote", ":cd:ura"];
+message.channel.send(array[Math.floor(Math.random() * array.length)]);
+console.log(array[Math.floor(Math.random() * array.length)]);
+}
+})
+
+
+
+//おみくじ
+client.on("message", message => {
+  if (message.author.id == client.user.id || message.author.bot) {
+    return;
+  }
+  if (
+    message.content.match(/!おみくじ/) ||
+    (message.isMemberMentioned(client.user) &&
+      message.content.match(/おみくじ/))
+  ) {
+    let arr = ["大吉", "吉", "凶", "中吉", "imposter", "変態"];
+    let weight = [5, 30, 10, 15, 20, 3];
+    lotteryByWeight(message.channel.id, arr, weight);
+  } else if (message.isMemberMentioned(client.user)) {
+    sendReply(message, "呼びましたか？");
+  }
+});
+
+function lotteryByWeight(channelId, arr, weight) {
+  let totalWeight = 0;
+  for (var i = 0; i < weight.length; i++) {
+    totalWeight += weight[i];
+  }
+  let random = Math.floor(Math.random() * totalWeight);
+  for (var i = 0; i < weight.length; i++) {
+    if (random < weight[i]) {
+      sendMsg(channelId, arr[i]);
+      return;
+    } else {
+      random -= weight[i];
+    }
+  }
+  console.log("lottery error");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
 
 if (process.env.DISCORD_BOT_TOKEN == undefined) {
   console.log("DISCORD_BOT_TOKENが設定されていません。");
